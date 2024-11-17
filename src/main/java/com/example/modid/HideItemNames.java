24136be25b23d897b08bd71d35348e1002601019
strubium <113206902+strubium@users.noreply.github.com>
2024-11-17 01:56:03 -0600

    yay

---
+++ src/main/java/com/example/modid/HideItemNames.java
@@ -0,0 +1,19 @@
+package com.example.modid;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = "hideitemnames", name = "Hide Item Names Mod", version = "1.0")
+public class HideItemNames {
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        // Access the GameSettings instance after Minecraft is initialized
+        GameSettings settings = Minecraft.getMinecraft().gameSettings;
+
+        // Disable held item tooltips
+        settings.heldItemTooltips = false;
+    }
+}
